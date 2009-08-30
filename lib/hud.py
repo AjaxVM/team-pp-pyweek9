fc8ab66a35672bbe55e8fe5eb9556ba806f8fc56
RoeBros <RoeBros@65a3a0a8-94c5-11de-a4e5-53967458c36f>
2009-08-30 20:38:59 +0000

    Added really bad feather model for testing purposes. Update mesh.py to handle models without nice materials Add a second level. Implement feathers and picking them up...

--- lib/hud.py
+++ lib/hud.py
@@ -12,17 +12,28 @@ class Hud(pyggel.scene.BaseSceneObject):
 
         self.hover_status = {}
         self.hover_status["door"] = self.font.make_text_image("Door - face and move next to it to open")
+        self.hover_status["feather"] = self.font.make_text_image("A feather! Get close to it and Right-click to pick it up")
 
         self.cur_text = None
+
+        self.feathers = self.font.make_text_image("Feathers: 0/3")
+        self.feathers.pos = (10, 50)
 
     def set_hover_status(self, text):
         self.cur_text = text
 
+    def update_feathers(self, have, max):
+        text = "Feathers: %s/%s"%(have, max)
+        if not self.feathers.text == text:
+            self.feathers.text = text
+
     def render(self):
         if self.cur_text:
             img = self.hover_status[self.cur_text]
-            x,y = 320, 50
+            x,y = 320, 25
             w,h = img.get_size()
             img.pos = x-w/2, y-h/2
             img.render()
             img.pos = x,y
+
+        self.feathers.render()
--- lib/main.py
+++ lib/main.py
@@ -3,6 +3,7 @@ import pyggel
 from pyggel import *
 
 import data, hud
+import random
 
 class LevelData(object):
     """WIP - for pathfinding and such later!"""
@@ -84,6 +85,30 @@ class HorzDoor(VertDoor, pyggel.geometry.Cube):
         self.off_height = 0
         self.orig_pos = _pos
 
+class Feather(pyggel.scene.BaseSceneObject):
+    objs = []
+    def __init__(self, pos):
+        if not self.objs:
+            self.objs.append(pyggel.mesh.OBJ(data.mesh_path("feather_test.obj")))
+        pyggel.scene.BaseSceneObject.__init__(self)
+        self.pos = pos
+
+        self.obj = random.choice(self.objs)
+
+    def picked(self):
+        self.game_hud.set_hover_status("feather")
+
+    def update(self):
+        x, y, z = self.rotation
+        y += 0.5
+        self.rotation = x, y, z
+
+    def render(self, camera=None):
+        self.update()
+        self.obj.pos = self.pos
+        self.obj.rotation = self.rotation
+        self.obj.render(camera)
+
 def get_geoms(level):
     tsize = 5.0
     fname = data.level_path("level%s.txt"%level)
@@ -101,6 +126,9 @@ def get_geoms(level):
 
     static = []
     dynamic = []
+
+    baddies = []
+    feathers = []
 
     commands = _data.split(":")
     commands = [i.strip() for i in commands if i]
@@ -147,12 +175,29 @@ def get_geoms(level):
                         dynamic.append(HorzDoor(tsize, door_tex, (x*tsize, -tsize/11, y*tsize)))
                     if cur == "*":
                         camera_pos = x*tsize, 0, y*tsize
+                    if cur == "~":
+                        feathers.append(Feather((x*tsize, 0, y*tsize)))
     return (pyggel.misc.StaticObjectGroup(static), dynamic,
+            baddies, feathers,
             camera_pos,
             fog_color, tile_set,
-            LevelData(map_grid, tsize))
+            LevelData(map_grid, tsize),
+            tsize)
 
-def play_level(level=1): #TODO: add controls for player data, like weapon, stats, etc.!
+class PlayerData(object):
+    def __init__(self):
+        self.max_hp = 100
+        self.cur_hp = 100
+
+        self.weapons = {}
+        self.kills = 0
+        self.cur_weapon = None
+
+    def add_weapon(self, wep_type, ammo=100):
+        self.weapons[wep_type] = ammo
+        self.cur_weapon = wep_type
+
+def play_level(level, player_data): #TODO: add controls for player data, like weapon, stats, etc.!
     camera = pyggel.camera.LookFromCamera((10,0,10))
     light = pyggel.light.Light((0,100,0), (0.5,0.5,0.5,1),
                                   (1,1,1,1), (50,50,50,10),
@@ -164,7 +209,7 @@ def play_level(level=1): #TODO: add controls for player data, like weapon, stats
 
     collidable = ["#"] #TODO: add other collidables!
 
-    static, dynamic, camera_pos, fog_color, tile_set, level_data = get_geoms(1)
+    static, dynamic, baddies, feathers, camera_pos, fog_color, tile_set, level_data, tsize = get_geoms(1)
     camera.set_pos(camera_pos)
     pyggel.view.set_fog_color(fog_color)
     pyggel.view.set_fog_depth(5, 60)
@@ -172,11 +217,16 @@ def play_level(level=1): #TODO: add controls for player data, like weapon, stats
     static.pickable = True
     scene.add_3d(static)
     scene.add_3d(dynamic)
+    scene.add_3d(baddies)
+    scene.add_3d(feathers)
 
     game_hud = hud.Hud()
     scene.add_2d(game_hud)
-    for i in dynamic:
+    for i in dynamic + feathers + baddies:
         i.game_hud = game_hud
+
+    game_hud.update_feathers(0, len(feathers))
+    have_feathers = 0
 
     clock = pygame.time.Clock()
     event = pyggel.event.Handler()
@@ -192,9 +242,23 @@ def play_level(level=1): #TODO: add controls for player data, like weapon, stats
         clock.tick(999)
         pyggel.view.set_title("FPS: %s"%clock.get_fps())
 
+        #Render first, since picking is done at this time, and we need that later!
+
+        pyggel.view.clear_screen()
+
+        pick = scene.render(camera, (320,240)) #make sure we only pick the center!
+        if hasattr(pick, "picked"):
+            pick.picked()
+        else:
+            game_hud.set_hover_status(None)
+
+        pyggel.view.refresh_screen()
+
+
+        #Now events!
         event.update()
         if K_ESCAPE in event.keyboard.hit:
-            return None
+            return "back"
 
         if event.mouse.motion[0]:
             camera.roty += event.mouse.motion[0] * 0.1
@@ -220,20 +284,19 @@ def play_level(level=1): #TODO: add controls for player data, like weapon, stats
                 camera.set_pos((camera.posx, camera.posy, camera.posz+new[2]))
             camera.roty *= -1
 
-        pyggel.view.clear_screen()
-
-        pick = scene.render(camera, (320,240)) #make sure we only pick the center!
-        if hasattr(pick, "picked"):
-            pick.picked()
-        else:
-            game_hud.set_hover_status(None)
-
-        pyggel.view.refresh_screen()
+        if "right" in event.mouse.hit:
+            if pick and isinstance(pick, Feather) and\
+               pyggel.math3d.get_distance(camera.get_pos(), pick.pos) < tsize*3:
+                have_feathers += 1
+                scene.remove_3d(pick)
+                game_hud.update_feathers(have_feathers, len(feathers))
 
 def main():
     pyggel.init()
 
-    play_level(1)
+    pData = PlayerData()
 
-    pyggel.quit()
-    return None
+    retval = play_level(1, pData)
+    if retval == "back":
+        pyggel.quit()
+        return None
