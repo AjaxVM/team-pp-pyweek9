e6a7bf4b0d78c0b43182299db34a7e952b99644c
RoeBros <RoeBros@65a3a0a8-94c5-11de-a4e5-53967458c36f>
2009-08-30 01:30:14 +0000

    Initial directory structure...

---
+++ lib/data.py
@@ -0,0 +1,10 @@
+import os
+
+def image_path(path):
+    return os.path.join("data", "image", path)
+
+def mesh_path(path):
+    return os.path.join("data", "mesh", path)
+
+def sound_path(path):
+    return os.path.join("data", "image", path)
