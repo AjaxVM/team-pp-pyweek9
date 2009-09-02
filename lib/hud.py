9174677f3138d8027180f9d6dcea0ae0ad305cc7
RoeBros <RoeBros@65a3a0a8-94c5-11de-a4e5-53967458c36f>
2009-09-02 15:44:22 +0000

    Implemented a texture for the door bar. Implemented hover text for badguys.

--- lib/hud.py
+++ lib/hud.py
@@ -19,6 +19,19 @@ class Hud(pyggel.scene.BaseSceneObject):
         self.hover_status["hp"] = self.font.make_text_image("A health {health}+20 pack! Right-click to take it")
         self.hover_status["ammo"] = self.font.make_text_image("An ammo {ammo}+25 pack! Right-click to take it")
 
+        colors = {"1//1//0.25":"fast",
+                  "0//1//0":"high damage",
+                  "0//0//1":"normal"}
+        shapes = {"quad":("A badguy (Level 1)", "- shoot it!"),
+                  "pyramid":("A badguy (Level 2)", "- shoot it!"),
+                  "dpyramid":("A badguy (Level 3)", "- shoot it!"),
+                  "cube":("A badguy (Level 4)", "- shoot it!"),
+                  "sphere":("A boss (Level 10)", "- watch out O_O"),
+                  "ellipsoid":("A boss (Level 15)", "- umm.. run?")}
+        for i in colors:
+            for x in shapes:
+                self.hover_status[i+"//"+x] = self.font.make_text_image(shapes[x][0]+" ("+colors[i]+") "+shapes[x][1])
+
         self.cur_text = None
 
         self.feathers = self.font.make_text_image("Feathers: 0/3")
