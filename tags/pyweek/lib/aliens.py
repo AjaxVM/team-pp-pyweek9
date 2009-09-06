06feb7be5541aa6f9158061f597ec476a107b995
RoeBros <RoeBros@65a3a0a8-94c5-11de-a4e5-53967458c36f>
2009-09-06 00:00:05 +0000

    Pyweek finish!

---
+++ tags/pyweek/lib/aliens.py
@@ -0,0 +1,279 @@
+import pyggel
+from pyggel import *
+
+import data, hud
+import random, math
+
+class AlienShot(pyggel.scene.BaseSceneObject):
+    obj = None
+    def __init__(self, pos, rotation, color, level_data, far=False):
+        if not AlienShot.obj:
+            AlienShot.obj = pyggel.image.Image3D(data.image_path("flash.png"))
+        pyggel.scene.BaseSceneObject.__init__(self)
+
+        self.collision_body = pyggel.math3d.Sphere(pos, 1)
+
+        if far:
+            dis = -3
+        else:
+            dis = -1
+        self.pos = pyggel.math3d.move_with_rotation(pos, rotation, dis)
+        self.rotation = rotation
+        self.level_data = level_data
+        self.color = color
+
+        self.scale_up = True
+        self.scale = 0.75
+        self.twist = 0
+
+        if self.color == (1,1,0.25,1):
+            self.damage = 2
+            self.speed = 3
+        if self.color == (0,1,0,1):
+            self.damage = 8
+            self.speed = 1
+        if self.color == (0,0,1,1):
+            self.damage = 4
+            self.speed = 2
+
+    def render(self, camera=None):
+        if self.scale_up:
+            self.scale += 0.6
+            if self.scale >= 4:
+                self.scale = 4
+                self.scale_up = False
+        else:
+            if self.scale > 0.25:
+                self.scale -= 0.5
+            else:
+                self.scale = 0.25
+                self.pos = pyggel.math3d.move_with_rotation(self.pos, self.rotation, -.75*self.speed)
+        if self.dead_remove_from_scene:
+            return
+        self.collision_body.set_pos(self.pos)
+        if self.level_data.get_at_uncon(self.pos[0], self.pos[2]) in self.level_data.collidable:
+            self.dead_remove_from_scene = True #kills object
+        self.obj.pos = self.pos
+        self.obj.scale = self.scale
+        self.obj.colorize = self.color
+        self.twist += 15
+        self.obj.rotation = (0,0,self.twist)
+        self.obj.render(camera)
+
+class Alien(pyggel.scene.BaseSceneObject):
+    objs = {}
+    texs = []
+    base_los_count = 0
+    def __init__(self, pos, kind="quad"):
+        if not Alien.objs:
+            Alien.objs["quad"] = pyggel.geometry.Quad(1)
+            Alien.objs["cube"] = pyggel.geometry.Cube(1, mirror=True)
+            Alien.objs["sphere"] = pyggel.geometry.Sphere(1)
+            Alien.objs["ellipsoid"] = pyggel.geometry.Sphere(1)
+            Alien.objs["ellipsoid"].scale = 1,2,1
+            Alien.objs["pyramid"] = pyggel.geometry.Pyramid(2)
+            Alien.objs["dpyramid"] = pyggel.geometry.DoublePyramid(2)
+            Alien.objs["boss"] = pyggel.geometry.DoublePyramid(5)
+            Alien.objs["boss"].scale = 1,0.5,1
+            Alien.texs.append(pyggel.data.Texture(data.image_path("alien_face1.png")))
+        pyggel.scene.BaseSceneObject.__init__(self)
+
+        self.pos = pos
+        self.rotation = (0,0,0)
+        self.obj = self.objs[kind]
+        self.tex = random.choice(self.texs)
+        self.kind = kind
+        self.color = random.choice(((1,1,0.25,1), (0,1,0,1), (0,0,1,1)))
+
+        if self.kind == "boss":
+            self.collision_body = pyggel.math3d.AABox(self.pos, 3.5)
+        else:
+            self.collision_body = pyggel.math3d.AABox(self.pos, (1.5,5,1.5))
+
+        self.got_hit = False
+        self.hit_grow = True
+        self.hit_scale = 0
+        self.hit_scale_inc = 0.1
+        all_hp = {"quad":4,
+                  "pyramid":6,
+                  "dpyramid":8,
+                  "cube":10,
+                  "sphere":15,
+                  "ellipsoid":20,
+                  "boss":200}
+
+        self.hp = all_hp[self.kind]
+        self.dead = False
+        if kind == "ellipsoid":
+            self.dead_scale = 2
+            self.dead_scale_dec = 0.2
+        else:
+            self.dead_scale = 1
+            self.dead_scale_dec = 0.1
+
+        self.shot_count = 0
+        self.noticed = False
+
+        self.stored_LOS = False
+        self.sLOS_count = Alien.base_los_count
+        Alien.base_los_count += 1
+        if Alien.base_los_count >= 25:
+            Alien.base_los_count = 0
+        self.connected_to = []
+
+    def build_connections(self, others, level_data):
+        for i in others:
+            if not (i in self.connected_to or i == self):
+                if abs(self.pos[0]-i.pos[0]) <= 8 and\
+                   abs(self.pos[2]-i.pos[2]) <= 8: #one-ish tile
+                    self.connected_to.append(i)
+                    if not self in i.connected_to:
+                        i.connected_to.append(self)
+
+                    for x in i.connected_to:
+                        if not (x in self.connected_to or x == self):
+                            self.connected_to.append(x)
+                    for x in self.connected_to:
+                        if not (x in i.connected_to or x == i):
+                            i.connected_to.append(x)
+
+    def LOS_to(self, topos, level_data, angle):
+        # this is probably issue 15 -- getting distance through the wall
+        # easy fix is take this out,  'proper' fix is to use some pathing alg to see if
+        # they are just around a corner or well beyond a wall
+        #if pyggel.math3d.get_distance(topos, self.pos) <= 5:
+        #    return True
+
+        if pyggel.math3d.get_distance(topos, self.pos) >= 50:
+            return False
+
+        pos = self.pos
+        see = True
+        for i in xrange(50):
+            pos = pyggel.math3d.move_with_rotation(pos, (0,angle,0), -4)
+            if pyggel.math3d.get_distance(topos, pos) <= 5:
+                return True
+            if level_data.get_at_uncon(pos[0], pos[2]) in level_data.collidable:
+                return False
+        return False
+
+    def make_aware(self):
+        self.game_hud.sfx.play_alien_alert()
+        self.noticed = True
+        for i in self.connected_to:
+            i.noticed = True
+
+    def picked(self):
+        self.game_hud.set_hover_status("%s//%s//%s//%s"%(self.color[0],self.color[1],self.color[2],self.kind))
+
+    def update(self, player_pos, level_data):
+        # disconnect with any killed peers -- probably to free up for garbage collection
+        for i in self.connected_to:
+            if i.dead_remove_from_scene:
+                self.connected_to.remove(i)
+
+        # spin model
+        x, y, z = self.rotation
+        y += 5
+        self.rotation = x,y,z
+        self.collision_body.set_pos(self.pos)
+
+        # rotate toward player
+        x = player_pos[0] - self.pos[0]
+        y = player_pos[2] - self.pos[2]
+        angle = math.atan2(-y, x)
+        angle = 90-(angle * 180.0)/math.pi
+
+
+        self.sLOS_count += 1
+        if self.sLOS_count >= 30:
+            self.stored_LOS = self.LOS_to(player_pos, level_data, angle)
+            self.sLOS_count = 0
+            if self.stored_LOS:
+                if random.randint(0,1):
+                    self.game_hud.sfx.play_alien_chatter()
+
+        if (not self.noticed):
+            if pyggel.math3d.get_distance(player_pos, self.pos) < level_data.tsize * 7:
+                if self.stored_LOS:
+                    self.make_aware()
+
+        # if noticed and have a line of sight
+        if self.noticed and self.stored_LOS:
+            # increment shot counter
+            self.shot_count += 1
+
+            if not self.kind == "boss":
+                # move toward the player every X ticks
+                engage_distance = 15    # aliens stop moving toward you when they get this close
+                movement = 0.5         # movement speed of the aliens
+                if self.shot_count % 5 == 0 and math.hypot(player_pos[0]-self.pos[0], player_pos[2]-self.pos[2]) > engage_distance:
+                    # move toward the player a little bit -- skip pathing  :)
+                    # could inject bugs that get you stuck in walls though probably, depending
+                    # on how the LOS raycast works
+                    # should probably implement A* pathing when I have more time and have looked at how the level data works
+            
+                    x,y,z = self.pos
+                    self.pos = x - (math.cos(math.radians(angle+90)) * movement), y, z - (math.sin(math.radians(angle+90)) * movement)
+                    self.collision_body.set_pos(self.pos)
+                
+            # boss shoots a little faster
+            if self.kind == "boss" and self.shot_count >= 50:
+                self.shot_count = 0
+                self.game_hud.sfx.alien_shoot()
+                return [AlienShot(self.pos, (0,angle+random.randint(-4,4),0),
+                                  random.choice(((1,1,0.25,1), (0,1,0,1), (0,0,1,1))),
+                                  level_data, True) for i in xrange(5)]
+            # regular alien shot
+            elif self.shot_count >= 75:
+                self.shot_count = 0
+                self.game_hud.sfx.alien_shoot()
+                return [AlienShot(self.pos, (0,angle,0), self.color, level_data)]
+
+        else:
+            self.shot_count = 45
+
+    def hit(self, damage):
+        self.game_hud.sfx.play_alien_hit()
+        self.make_aware()
+        self.stored_LOS = True
+        self.got_hit = True
+        self.hp -= damage
+        if self.hp <= 0:
+            self.game_hud.sfx.play_alien_die()
+            if random.randint(0,1):
+                self.game_hud.sfx.play_player_kill()
+            self.dead = True
+
+    def render(self, camera=None):
+        if self.dead:
+            self.dead_scale -= self.dead_scale_dec
+            if self.dead_scale <= 0:
+                self.dead_remove_from_scene = True
+                return
+            oscale = self.dead_scale
+        elif self.got_hit:
+            self.obj.outline = True
+            if self.hit_grow:
+                self.hit_scale += self.hit_scale_inc
+                if self.hit_scale >= 1:
+                    self.hit_scale = 1
+                    self.hit_grow = False
+            else:
+                self.hit_scale -= self.hit_scale_inc*2
+                if self.hit_scale <= 0:
+                    self.hit_scale = 0
+                    self.got_hit = False
+                    self.hit_grow = True
+        else:
+            self.obj.outline = False
+        self.obj.pos = self.pos
+        x, y, z = self.rotation
+        self.obj.rotation = x, y, z
+        self.obj.texture = self.tex
+        self.obj.colorize = self.color
+
+        oscale = self.dead_scale
+        if self.kind == "quad": oscale += self.hit_scale
+        self.obj.scale = 1+self.hit_scale, oscale, 1+self.hit_scale
+        self.obj.render(camera)
---
+++ tags/pyweek/lib/hud.py
@@ -0,0 +1,329 @@
+
+import pyggel
+from pyggel import *
+
+import data
+import time
+
+import sfx
+        
+
+class Hud(pyggel.scene.BaseSceneObject):
+    def __init__(self):
+        pyggel.scene.BaseSceneObject.__init__(self)
+
+        self.font = pyggel.font.Font()
+        self.big_font = pyggel.font.Font(None, 45)
+        self.font.add_image("{health}", pyggel.image.Image(data.image_path("hp.png")))
+        self.font.add_image("{ammo}", pyggel.image.Image(data.image_path("ammo.png")))
+        self.font.add_image("{feather}", pyggel.image.Image(data.image_path("feather.png")))
+
+        self.big_font.add_image("{health}", pyggel.image.Image(data.image_path("hp.png")))
+        self.big_font.add_image("{ammo}", pyggel.image.Image(data.image_path("ammo.png")))
+        self.big_font.add_image("{feather}", pyggel.image.Image(data.image_path("feather.png")))
+
+        self.hover_status = {}
+        self.hover_status["door"] = self.font.make_text_image("Door - face and move next to it to open", color=(0,0,0,1))
+        self.hover_status["feather"] = self.font.make_text_image("A feather! Get close to it and Right-click to take", color=(0,0,0,1))
+        self.hover_status["chicken"] = self.font.make_text_image("Chicken! Move to and right-click to talk to her!", color=(0,0,0,1))
+        self.hover_status["shotgun"] = self.font.make_text_image("A shotgun! Better grab that - Right-click to take", color=(0,0,0,1))
+        self.hover_status["handgun"] = self.font.make_text_image("A handgun, better than nothing - Right-click to take", color=(0,0,0,1))
+        self.hover_status["chaingun"] = self.font.make_text_image("A chaingun, yeah baby! - Right-click to take", color=(0,0,0,1))
+        self.hover_status["plasma gun"] = self.font.make_text_image("A plasma gun, same energy as the aliens use! - Right-click", color=(0,0,0,1))
+        self.hover_status["chicken gun"] = self.font.make_text_image("A chicken gun... WTH - well, better try it! - Right-click", color=(0,0,0,1))
+        self.hover_status["hp"] = self.font.make_text_image("A +20 {health} pack! Right-click to take", color=(0,0,0,1))
+        self.hover_status["ammo"] = self.font.make_text_image("A {ammo} pack! Right-click to take", color=(0,0,0,1))
+        self.hover_status["starting_console"] = self.font.make_text_image("Alien Console - move to and Right-click to use!", color=(0,0,0,1))
+
+        colors = {"1//1//0.25":"fast",
+                  "0//1//0":"high damage",
+                  "0//0//1":"normal"}
+        shapes = {"quad":("A badguy (Level 1)", "- shoot it!"),
+                  "pyramid":("A badguy (Level 2)", "- shoot it!"),
+                  "dpyramid":("A badguy (Level 3)", "- shoot it!"),
+                  "cube":("A badguy (Level 4)", "- shoot it!"),
+                  "sphere":("An enforcer (Level 5)", "- watch out O_O"),
+                  "ellipsoid":("An enforcer (Level 6)", "careful there!"),
+                  "boss":("A boss (Level 10)", "The Controller - kill him!")}
+        for i in colors:
+            for x in shapes:
+                self.hover_status[i+"//"+x] = self.font.make_text_image(shapes[x][0]+" ("+colors[i]+") "+shapes[x][1],
+                                                                        color=(0,0,0,1))
+
+        self.cur_text = None
+
+        self.feathers = self.big_font.make_text_image("{feather} 0/3", color=(0,0,0,1))
+        self.feathers.pos = (10, 5)
+
+        self.hp = self.big_font.make_text_image("{health} 100", color=(0,0,0,1))
+        self.hp.pos = (130, 5)
+        self.weapon = self.font.make_text_image("Weapon: None", color=(0,0,0,1))
+        self.weapon.pos = (260, 10)
+        self.weapon.scale = (1,1.5,1)
+        self.ammo = self.big_font.make_text_image("{ammo} 100", color=(0,0,0,1))
+        self.ammo.pos = (505, 5)
+
+        self.event_handler = pyggel.event.Handler()
+
+        #create gui apps for various events!
+        self.main_app = pyggel.gui.App(self.event_handler)
+        self.main_app.theme.load(data.gui_path("theme.py"))
+
+        #set up later theme/font usage
+        self.core_theme = self.main_app.theme
+        self.core_fonts = self.main_app.fonts
+        #any apps that are created later:
+        #   app.theme = self.core_theme
+        #   app.update_fonts(self.core_fonts)
+
+        intro_qa = [("Hmm, this looks like a radio...\nmy nerves are fried...\nmaybe they have country 122.3 here...","<Hit some buttons>"),
+                   ("BZZT... Squad 4 to sector 12...\numm sir, there's someone else on the line!!\n"+\
+                    "Communication is coming from the alien mother-ship!", "...uhm, oops... I'm not an alien..."),
+                   ("Of course you're not an alien, they can't speak...\nidentify yourself then...", "I'm Farmer Bob"),
+                   ("Oh. I see - is your chicken there?", "My chicken?!?! umm, no..."),
+                   ("Ok, then I'd better explain.\nWe're at war with a race of extra-dimensional aliens.\n"+\
+                    "These aliens can't completely fit into our dimension,\ninstead they have to confine themselves\nto geometric shapes.",
+                    "<continue>"),
+                   ("Your chicken is an advanced mutated commando,\nand was fowl-napped while investigating\nthat crop-circle.\n"+\
+                    "Oh, btw - that crop circle was actually\nthe outline of the alien mothership.\n"+\
+                    "Anyway, we lost communication with Chicken\nshortly after she got on board.\nok...\n"+\
+                    "Now to explain your mission.",
+                    "My WHAT?!?!"),
+                   ("Your mission, please pay attention.", "*whispers* should have stayed in the house...")]
+        cont_qa = [("Your objectives are simple, destroy the alien ship\nand rescue Chicken.",
+                    "Blow the ship? How am I supposed to do that?"),
+                   ("Chicken will know what to do...",
+                    "Wow, that sounds pretty easy..."),
+                   ('"Easier said than done" could never be more true,\nthe controller is exceedinly strong.',
+                    "Umm, then how am I supposed to kill it?"),
+                   ("Chicken had several weapons when she entered the ship,\nand would not have gone down without a fight.\n"+\
+                    "She probably dropped weapons throughout the ship.\nLuckily, the aliens don't understand\n"+\
+                    "the concept of our weapons, and probably ignored them.",
+                    "Nice, an enemy even dumber than me!\nSo how do I find Chicken and the controller?"),
+                   ("Ahem, Chicken probably lost a few feathers\nfighting her captors.\nFollow and collect them.\n"+\
+                    "Once you have all the feathers in an area\na teleporter chip will be activated\nto the next level.",
+                    "Interesting... anything else I should know?"),
+                   ("That should be plenty - good luck Farmer!", "Then let's do this!\n...\nhey! wait a second\nwhy do I have to do this?"),
+                   ("Because, we haven't managed to get a single\nsoldier onto that mother ship yet!\n"+\
+                    "So, we aren't gonna let this opportunity\nescape us!\n"+\
+                    "Not to mention, you and Chicken\nare linked... somehow.\nGood luck soldier.", "ok... better get moving...")]
+
+        chick_qa = [("Ahh, you did find my trail of\nfeathers then!","Umm, yes - nice to see you too"),
+                   ("Of course...\nthank you for rescuing me", "...uhm, no problem..."),
+                   ("OK, well then, let's blow this joint!", "Yeah, oh wait..."),
+                   ("What?", "The commander guy said the ship would\nfall apart once the controller\nwas killed"),
+                   ("Yes... well, he was incorrect...\nbut no problem...\nI've already laid some charges...", "How - weren't you captured?"),
+                   ("Haha - yes, but I escaped once\nyou came in and started shooting\n"+\
+                    "and laid the charges as you fought...", "I didn't see you..."),
+                   ("You never saw me training either...\nnot surprising you missed me\nin the heat of battle", "Oh... right..."),
+                    ("Alrighty then, let's go home, Farmer...", "<continue>")]
+
+        self.console_app_discussions_intro = intro_qa + cont_qa
+        self._console_swap_point = len(intro_qa)
+        self.console_last_index = 0
+        self._console_repeat_stor = {} #so we don't have this issue again!
+
+        self.chick_app_discussions = chick_qa
+        self.chick_last_index = 0
+        self.played_chick = False
+
+        self.active_app = None
+
+        self.app_binding = {"console":self.console_app_discussions_intro[0],
+                            "chicken":self.chick_app_discussions[0]}
+        self.grab_events = False
+
+
+        #target image
+        self.target = pyggel.image.Image(data.image_path("target.png"),
+                                         pos=(320-32, 240-32))
+        self.target_active = pyggel.image.Image(data.image_path("target_over.png"),
+                                                pos=(320-32,240-32))
+
+        self.hud_image = pyggel.image.Image(data.image_path("hud1.png"))
+        self.logo_image = pyggel.image.Image(data.image_path("chickenstein_logo.png"))
+        self.logo_image.colorize = (1,1,1,0.5)
+
+        self.ouch_image = pyggel.image.Image(data.image_path("ouch.png"))
+        self.ouch_image.colorize = (1,1,1,0)
+
+        self.paused = self.big_font.make_text_image("Paused - hit 'p' to start again...", color=(0,0,0,1))
+        self.paused.pos = 320-self.paused.get_width()/2, 240-self.paused.get_height()/2
+        self.paused.visible = False
+
+        #for 3d sounds...
+        self.sfx = sfx.SFX()
+        self.blink_count = 0
+        self.blink_ammo = False
+        self.blink_hp = False
+
+    def reset(self):
+        self.ouch_image.colorize = (1,1,1,0)
+        self.gui_inactive()
+
+    def got_hit(self):
+        self.ouch_image.colorize = 1,1,1,1
+
+    def _set_active_app_internal_cadi_next(self):
+        app = self.console_last_index
+        if app < len(self.console_app_discussions_intro):
+            self.console_last_index += 1
+
+            if app in self._console_repeat_stor:
+                app = self._console_repeat_stor[app]
+            else:
+                label = self.console_app_discussions_intro[app]
+
+                app = pyggel.gui.App(self.event_handler)
+                app.theme = self.core_theme
+                app.update_fonts(self.core_fonts)
+                app.packer.packtype = "center"
+                pyggel.gui.Label(app, label[0], font_color=(0,0,0,1), font_color_inactive=(0,0,0,1))
+                pyggel.gui.NewLine(app)
+
+                pyggel.gui.Button(app, label[1], callbacks=[lambda:self._set_active_app_internal_cadi_next()])
+                self._console_repeat_stor[self.console_last_index-1] = app
+
+            self.active_app = app
+            app.activate()
+        else:
+            self.console_last_index = self._console_swap_point
+            self.gui_inactive()
+
+    def _set_active_app_internal_chick_next(self):
+        app = self.chick_last_index
+        if app < len(self.chick_app_discussions):
+            self.chick_last_index += 1
+
+            label = self.chick_app_discussions[app]
+
+            app = pyggel.gui.App(self.event_handler)
+            app.theme = self.core_theme
+            app.update_fonts(self.core_fonts)
+            app.packer.packtype = "center"
+            pyggel.gui.Label(app, label[0], font_color=(0,0,0,1), font_color_inactive=(0,0,0,1))
+            pyggel.gui.NewLine(app)
+
+            pyggel.gui.Button(app, label[1], callbacks=[lambda:self._set_active_app_internal_chick_next()])
+
+            self.active_app = app
+            app.activate()
+        else:
+            self.console_last_index = 0
+            self.gui_inactive()
+            self.played_chick = True
+
+    def gui_inactive(self):
+        pygame.event.set_grab(1)
+        pygame.mouse.set_visible(0)
+        self.event_handler.gui = None #turn off all guis
+        self.active_app = None
+        self.grab_events = False
+
+    def gui_active(self):
+        pygame.event.set_grab(0)
+        pygame.mouse.set_visible(1)
+        self.active_app.activate()
+        self.grab_events = True
+        pygame.mouse.set_pos((320,240))
+
+    def set_gui_app(self, name):
+        if name in self.app_binding:
+            if name == "console":
+                self._set_active_app_internal_cadi_next()
+                self.gui_active()
+            elif name == "chicken":
+                self._set_active_app_internal_chick_next()
+            else:
+                self.active_app = self.app_binding[name]
+            self.gui_active()
+        else:
+            self.gui_inactive()
+
+    def set_hover_status(self, text):
+        self.cur_text = text
+
+    def update_feathers(self, have, max):
+        text = "{feather} %s/%s"%(have, max)
+        if not self.feathers.text == text:
+            self.feathers.text = text
+
+    def update_hp(self, amount):
+        if amount <= 20:
+            self.blink_hp = True
+            self.hp.color = (1,0,0,1)
+        else:
+            self.blink_hp = False
+            self.hp.color = (0,0,0,1)
+        text = "{health} %s"%amount
+        if not self.hp.text == text:
+            self.hp.text = text
+
+    def update_ammo(self, amount):
+        if amount <= 10:
+            self.blink_ammo = True
+            self.ammo.color = (1,0,0,1)
+        else:
+            self.blink_ammo = False
+            self.ammo.color = (0,0,0,1)
+        text = "{ammo} %s"%amount
+        if not self.ammo.text == text:
+            self.ammo.text = text
+
+    def update_weapon(self, weapon):
+        text = "Weapon: %s"%weapon
+        if not self.weapon.text == text:
+            self.weapon.text = text
+
+    def render(self):
+        self.sfx.update()
+        if self.active_app:
+            self.active_app.render()
+
+        else:
+            self.blink_count += 1
+            if self.blink_count >= 15:
+                self.blink_count = 0
+            if self.blink_count >= 5:
+                blink = True
+            else:
+                blink = False
+
+            if self.blink_ammo:
+                self.ammo.visible = blink
+            else:
+                self.ammo.visible = True
+            if self.blink_hp:
+                self.hp.visible = blink
+            else:
+                self.hp.visible = True
+            a,b,c,d = self.ouch_image.colorize
+            if d <= 0:
+                d = 0
+
+            if d:
+                self.ouch_image.render()
+                d -= 0.025
+                self.ouch_image.colorize = a,b,c,d
+            self.logo_image.render()
+            self.hud_image.render()
+
+            if self.cur_text:
+                img = self.hover_status[self.cur_text]
+                x,y = 320, 65
+                img.pos = x-img.get_width()/2, y
+                img.render()
+                img.pos = x,y
+                self.target_active.render()
+            else:
+                self.target.render()
+
+            self.feathers.render()
+            if self.hp.visible:
+                self.hp.render()
+            if self.ammo.visible:
+                self.ammo.render()
+            self.weapon.render()
+
+            if self.paused.visible:
+                self.paused.render()
